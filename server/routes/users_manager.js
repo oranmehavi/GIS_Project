ef9a518f09de64df619934987e172ad60e3f713b
lolsniperftw <41841264+lolsniperftw@users.noreply.github.com>
2023-04-09 01:00:27 +0300

    Change username to email and also check password

--- server/routes/users_manager.js
+++ server/routes/users_manager.js
@@ -4,11 +4,16 @@ const client = require('../scripts/db');
 
 router.get('/login', async function(req,res) {
     try {
-        const result = await client.query("SELECT user_name FROM users WHERE user_name= $1", [req.body.username]);
+        const result = await client.query("SELECT user_name FROM users WHERE email= $1", [req.body.email]);
         if (result.rows.length === 0) {
             return res.status(500).json({message: 'User does not exist'});
         }
-        return res.status(200).json({message: 'Login is successful'});
+        if (result.rows[0].password === req.body.password) {
+            return res.status(200).json({message: 'Logged in successfully'});
+        }
+        else {
+            return res.status(500).json({message: 'Password does not match'});
+        }
     }
     catch (e) {
         return res.status(500).json({message: e.message});
