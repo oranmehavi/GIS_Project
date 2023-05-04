87fadf4ed335410aae6f9a2423b5a043480cd9e9
Gal Bachar <gal.bachar@cymotive.com>
2023-05-04 18:23:10 +0300

    map integration

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,27 @@ import {Login} from "./Login";
 import {Register} from "./Register";
 import Navbar from './Navbar';
 import Home from './Home';
+import Map from './map.js'
 import { Route,Routes} from 'react-router-dom';
 
 
  function App() { 
  let componet
    return(
+    <div>   
      <>
       <Navbar/>
      <div className= "container">{componet}</div> 
       <Routes>
        <Route path = "Home" element={<Home />}/> 
+       <Route path = "Map" element={<Map />}/> 
        <Route path = "login" element={<Login />}/> 
        <Route path = "register" element={<Register />}/> 
       </Routes> 
 
-  
+
       </>
+      </div>
 //    )
 //  const [currentForm, setCurrentForm] = useState('login');
 //  const togglwForm = (forname) => {
