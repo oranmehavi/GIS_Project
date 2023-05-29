90b0c4ad60d47bb7f2b25503ca26b99f80af671f
lolsniperftw <41841264+lolsniperftw@users.noreply.github.com>
2023-05-29 20:36:52 +0300

    Also send the years in the range

--- server/routes/getdata.js
+++ server/routes/getdata.js
@@ -11,17 +11,10 @@ router.post('/getdata', async function (req, res) {
     try {
         result = await client.query("SELECT id FROM cities WHERE name= $1", [req.body.city_name]);
         let city_id = result.rows[0].id;
-        result = await client.query("SELECT * FROM historical_data WHERE city_id= $1 AND year= $2",
-                                        [city_id, year1]);
-        year1_pop = result.rows[0].population;
-        result = await client.query("SELECT * FROM historical_data WHERE city_id= $1 AND year= $2",
-            [city_id, year2]);
-        year2_pop = result.rows[0].population;
+        result = await client.query("SELECT * FROM historical_data WHERE city_id= $1 AND year BETWEEN $2 AND $3",
+                                        [city_id, year1, year2]);
 
-        return res.status(200).json([
-            {year: year1, population: year1_pop},
-            {year: year2, population: year2_pop}
-        ]);
+        return res.status(200).json(result.rows);
     }
     catch (e) {
         return res.status(500).json({message: e.message});
