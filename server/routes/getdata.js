f36167892286f6b21fb1ac200207ab22afec04f2
lolsniperftw <41841264+lolsniperftw@users.noreply.github.com>
2023-05-29 20:47:28 +0300

    Add check for input

--- server/routes/getdata.js
+++ server/routes/getdata.js
@@ -10,9 +10,14 @@ router.post('/getdata', async function (req, res) {
     year2 = req.body.year2;
     try {
         result = await client.query("SELECT id FROM cities WHERE name= $1", [req.body.city_name]);
+        if (result.rows.length === 0)
+            return res.status(500).json({message: "City does not exist"});
         let city_id = result.rows[0].id;
         result = await client.query("SELECT * FROM historical_data WHERE city_id= $1 AND year BETWEEN $2 AND $3",
                                         [city_id, year1, year2]);
+
+        if (result.rows.length === 0)
+            return res.status(500).json({message: "Year does not exist or range is incorrect"});
 
         return res.status(200).json(result.rows);
     }
