a83980af8ef95574b336dba4e41f6532b0657139
lolsniperftw <41841264+lolsniperftw@users.noreply.github.com>
2024-01-07 23:36:38 +0200

    Fix for build

--- client/src/Register.js
+++ client/src/Register.js
@@ -8,7 +8,7 @@ export const Register = (props) => {
      const [email,setEmail] = useState('');
      const [pass,setPass]= useState('');
      const [city, setCity] = useState('');
-     const Navigate = useNavigate();
+     const navigate = useNavigate();
 
      const handleSbmit = (e) => {
         e.preventDefault();
@@ -43,35 +43,32 @@ export const Register = (props) => {
       };
 
      return (
-        <div className='auth-form-container'>
-        <h1> Population Growth</h1> 
-        <h2>Register</h2>
-        <form className='register-form'onSubmit={handleSbmit}>
-            <label htmlFor="Full Name">Name:</label>
-            <input value={name} type="full name"
-             placeholder="full name" id="full name" name="full name"
-             onChange={(e) => Setname(e.target.value)}
-             />
-            <label htmlFor="email">Email:</label>
-            <input value={email} type="email" placeholder="youremail@gmail.com"
-             id="email" name="email"
-             onChange={(e) => setEmail(e.target.value)}
-             />
-            <label htmlFor="password">Password:</label>
-            <input value={pass} type="password" placeholder="********"
-             id="password" name="password"
-             onChange={(e) => setPass(e.target.value)}
-             />
-            <label htmlFor="livingcity">city:</label>
-            <input value={city} type="livingcity" placeholder="city" 
-            id="livingcity" name="livingcity"
-            onChange={(e) => setCity(e.target.value)}
-            />
-            <button type="submit"> Register </button>
+      <div className='auth-form-container'>
+      <h1> Population Growth</h1> 
+      <h2>Register</h2>
+      <form className='register-form'onSubmit={handleSbmit}>
+          <label htmlFor="Full Name">Name:</label>
+          <input value={username}
+           placeholder="Username" id="username" name="username"
+           onChange={(e) => Setname(e.target.value)}
+           required={true}/>
+          <label htmlFor="email">Email:</label>
+          <input value={email} type="email" placeholder="youremail@gmail.com"
+           id="email" name="email"
+           onChange={(e) => setEmail(e.target.value)}
+           required={true}/>
+          <label htmlFor="password">Password:</label>
+          <input value={pass} type="password" placeholder="********"
+           id="password" name="password"
+           onChange={(e) => setPass(e.target.value)}
+           required={true}/>
+          <label htmlFor="livingcity">city:</label>
+          <input value={city} placeholder="city" 
+          id="livingcity" name="livingcity"
+          onChange={(e) => setCity(e.target.value)}
+          required={true}/>
+          <button type="submit"> Register </button>
         </form>
-        <button className='link-btn' onClick={() => props.onFormSwitch('login')} >
-            Already have an account? Register here</button>
-
         </div>
         )
 
