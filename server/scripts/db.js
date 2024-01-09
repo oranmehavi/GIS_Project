4701f66956cb7b7385656c0909ce5c3a6a10255e
lolsniperftw <41841264+lolsniperftw@users.noreply.github.com>
2024-01-09 23:23:13 +0200

    Enable SSL for DB connection

--- server/scripts/db.js
+++ server/scripts/db.js
@@ -3,7 +3,8 @@ const { Pool } = require("@neondatabase/serverless");
 
 
 const pool = new Pool ({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 });
 
 
